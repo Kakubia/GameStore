7b80d87e63d9c5fce793053a93b28f7e37f7a8d8
Kakubia <kakubia@gmail.com>
2022-06-01 23:44:16 -0300

    Created the project to practice CRUD and security

---
+++ src/main/java/com/generation/gamestore/repository/ProductRepository.java
@@ -0,0 +1,20 @@
+package com.generation.gamestore.repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.generation.gamestore.model.Product;
+
+
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+	public List<Product> findAllByNameContainingIgnoreCase(@Param("name") String name);
+	
+	public List<Product> findByPrice(@Param("price") BigDecimal price);
+}
